70bdc5de0e282a649d783112fc557232afa551ff
webdiego <webdiegomassarini@gmail.com>
2022-12-08 18:07:46 +0100

    init

---
+++ pages/api/generateImage.ts
@@ -0,0 +1,27 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+const { Configuration, OpenAIApi } = require('openai');
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  let requestImage = req.body.text;
+  console.log(requestImage);
+  try {
+    const response = await openai.createImage({
+      prompt: requestImage,
+      n: 1,
+      size: '512x512',
+    });
+    let url = response.data.data[0].url;
+    res.status(200).json({ url: url });
+  } catch (error) {
+    if (error) {
+      res.status(400).json({ error: `${error}` });
+    } else {
+      res.status(400).json({ error: `${error}` });
+    }
+  }
+}
