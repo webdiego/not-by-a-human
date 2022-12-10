501f8dc6af1ddd60fa83ede2a2bb439d76a21800
webdiego <webdiegomassarini@gmail.com>
2022-12-10 13:44:46 +0100

    update(made)

--- components/Made.tsx
+++ components/Made.tsx
@@ -2,18 +2,19 @@ import React from 'react';
 import Image from 'next/image';
 import Next from '../public/next.svg';
 import Ts from '../public/ts.svg';
+import OpenAi from '../public/openAi.svg';
 
 export default function Made() {
   return (
     <div
       className={
-        'bg-black text-white w-full font-light self-center text-center text-xs flex items-center justify-center pt-5 space-x-2'
+        'bg-black text-white w-full font-light self-center text-center text-xs flex items-center justify-center pt-5 space-x-3'
       }
     >
       <p className={'text-center text-sm'}>Made with </p>
-      <Image src={Next} width={60} alt="next" />
-      <p>and </p>
+      <Image src={Next} width={60} alt="next" />,
       <Image src={Ts} width={20} alt="next" />
+      <Image src={OpenAi} width={60} alt="next" />
     </div>
   );
 }
